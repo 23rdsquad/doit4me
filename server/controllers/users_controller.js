2bab192749cddbda2b5a6d962bee615aa17f2007
Sergey Sherstobitov <in43sh@gmail.com>
2019-01-31 17:23:42 -0800

    created a controller

---
+++ server/controllers/users_controller.js
@@ -0,0 +1,63 @@
+const Users = require('../models').items;
+const models = require ('../models');
+
+module.exports = {
+  createItem: (req, res, next) => {
+    console.log(req.body)
+    // res.status(200).json({ status: 'Created one user' })
+    const { name } = req.body;
+    return Users
+    .create({ name })
+    .then(user => res.status(200).json({ status: 'Created one user', user }))
+    .catch(error => console.log(error));
+  },
+
+  getAllItems: (req, res, next) => {
+    return Users
+    .findAll({paranoid: false})
+    .then(items => res.status(200).json({ status: 'Retrieved all items', items }))
+    .catch(error => console.log(error));
+  },
+
+  getSingleItem: (req, res, next) => {
+    const { id } = req.params;
+    return Users
+    .findById(id)
+    .then(user => res.status(200).json({ status: 'Retrieved one user', user }))
+    .catch(error => console.log(error));
+  },
+
+  updateItem: (req, res, next) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    return Users
+    .findById(id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({ message: 'User not found' })
+      }
+      return user
+      .update({
+        name: name
+      })
+      .then(user => res.status(200).json({ status: 'Updated one user', user }))
+      .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
+  },
+
+  destroyItem: (req, res, next) => {
+    const { id } = req.params;
+    return Users
+    .findById(id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({ message: 'User not found' })
+      }
+      return user
+      .destroy()
+      .then(() => res.status(200).json({ status: 'Deleted one user', user }))
+      .catch(error => console.log(error));
+    })
+  },
+};
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 const users_controller = require('./controllers/items_controller');
 
-app.post('/item/create', items_controller.createItem);
+app.post('/user/create', items_controller.createItem);
 app.get('/items', items_controller.getAllItems);
 
 const port = process.env.BACKEND_PORT;
