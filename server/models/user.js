080b632abd0997f6ca1596db74ed3e754b493617
Sergey Sherstobitov <in43sh@gmail.com>
2019-02-14 14:50:47 -0800

    changed user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,38 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    nickname: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    ip: DataTypes.STRING, // has to be array
-    location: DataTypes.INTEGER,
-    personal: DataTypes.STRING,
-    rating: DataTypes.FLOAT,
-    photo: DataTypes.STRING // for now string, later will be base64
+    nickname: {
+      allosNull: false,
+      type: DataTypes.STRING
+    },
+    password: {
+      allosNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allosNull: false,
+      type: DataTypes.STRING
+    },
+    ip: {
+      allosNull: false,
+      type: DataTypes.STRING // has to be array
+    },
+    location: {
+      allosNull: false,
+      type: DataTypes.INTEGER
+    },
+    personal: {
+      allosNull: false,
+      type: DataTypes.STRING // has to be array
+    },
+    rating: {
+      allosNull: false,
+      type: DataTypes.FLOAT
+    },
+    photo: {
+      allosNull: false,
+      type: DataTypes.STRING // for now string, later will be base64
+    }
   }, {
     timestamps: true,
     paranoid: true
