6d5243e59e8011a38465eff0a9f6a17b890a68d6
BazT <bazetug@gmail.com>
2019-04-22 23:10:31 -0700

    rebuilding singin page

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,41 +1,57 @@
 import React, { Component } from "react"
 import "./styles/SignIn.css"
+import axios from 'axios';
 
 export default class SignIn extends Component {
   state ={
     words: '',
-    email: '',
+    nickname: '',
     password: ''
   }
+  componentWillMount() {
+    console.log('ready!')
+  }
   handleChange = (event) => {
-    this.setState({ words: event.target.value })
+    console.log(event)
+    // this.setState({ words: event.target.value })
+  }
+  login(){ // Login as admin
+    const nickname = this.state.nickname
+    const password = this.state.password
+    axios.post('/login', {
+      nickname,
+      password
+    }).then(response => {
+      console.log(response)
+    }).catch(error => {
+      alert("Wrong email or password");
+    })
   }
   render() {
     return (
-            <div className="card">
+            <div className="SignIn-card">
               <div className="card-content black-text">
-                <span className="card-title">Sign In</span>
-                    <div className="row card-row">
-                      <div className="input-field col s8">
-                        <input placeholder="TaskCompleter" type="text" id="email" className="validate" onChange = {this.handleChange}/>
-                        <label className="active" for="email">Email</label>
-                      </div>
-                    </div>
-                    <div className="row card-row">
-                      <div className="input-field col s8">
-                        <input placeholder="Password" id="password" type="text" className="validate" onChange = {this.handleChange}/>
-                        <label className="active" for="password">Password</label>
-                      </div>
-                    </div>
-                    <section>
-                      <a href="/">Forgot Login</a>
-                      <span>|</span>
-                      <a href="/">Forgot Password</a>
-                    </section>
+                <h3>Login</h3>
+                
+                <div className="input-field col s8">
+                  <input  type="text" id="nickname" className="validate" onChange = {this.handleChange(this)}/>
+                  <label className="active" htmlFor="nickname">Nickname</label>
+                </div>
+                <div className="input-field col s8">
+                  <input  id="password" type="password" className="validate" onChange = {this.handleChange(this)}/>
+                  <label className="active" htmlFor="password">Password</label>
+                </div>
+                <div>
+                  <div className="center-align SignIn-buttons-box-narrow">
+                    <a href="/" className="collection-item pulse">Forgot Login</a>
+                    <a href="/" className="collection-item pulse">Forgot Password</a>
+                  </div>
+                </div>
+                
               </div>
-              <div className="card-action">
-                <a href="/">Submit</a>
-                <a href="/">Sign up</a>
+              <div className="SignIn-buttons-box">
+                <button className="btn waves-effect waves-light z-depth-3" onClick={() => { this.login() }}>Submit</button>
+                <button  className="btn waves-effect blue darken-1 z-depth-3" >Register</button>
               </div>
             </div>
     );
