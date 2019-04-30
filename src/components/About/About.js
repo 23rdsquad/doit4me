8117d948fd3e471037213a192334fe25aefb3736
BazT <bazetug@gmail.com>
2019-04-29 22:25:52 -0700

    switching back to materialize

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,170 +7,122 @@ import world from '../../assets/icons/world.png'
 import notebook from '../../assets/notebook.jpg'
 import map from '../../assets/map.jpg'
 import painting from '../../assets/painting.jpg'
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
-/*
+
+
 export default class About extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      classes: PropTypes.object.isRequired
+      search_input: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      search_input: event.target.value
+    })
+  }
+  createClicked(){
+    console.log("createClicked()")
+  }
+  searchClicked(){
+
+    console.log(this.state.search_input)
   }
   
   render() {
-    return (
-      
-    );
+    
+    
+      return (
+        <main className="About">
+            <div>
+              <div className="hero-image">
+                <div className='hero-text'>
+                  <div className="center-align">We will let you free from your duties.</div>
+                    <h4 className="center-align">Let someone else do your job.</h4>
+                    <form className="col s12">
+                      <div className="input-field col s6">
+                        <input id="about-input" type="text" className="about-input" onChange={event => this.handleChange(event)} />
+                        <label  className="center-align about-input-label" htmlFor="about-input">What do you need?</label>
+                      </div>
+                    </form>
+                    <div className="about-buttons-space-around">
+                        <button className="waves-effect green lighten-1 btn-large"  onClick={() => this.createClicked()}>Create</button>
+                        <button className="waves-effect blue lighten-3 btn-large" onClick={() => this.searchClicked()}>Search</button>
+                    </div>
+                </div>
+              </div>
+            </div>  
+             
+    
+            <section>
+              <div>
+                <img src={check} alt="check" />
+                <h5>Reliable Taskers</h5>
+                <p>Task perfomers verified through us.</p>
+              </div>
+              <div>
+                <img src={clipboard} alt="clipboard" />
+                <h5>Amazing Reviews</h5>
+                <p>We ensure every customer receives satifactory work.</p>
+              </div>
+              <div>
+                <img src={target} alt="target" />
+                <h5>Perfect Score</h5>
+                <p>There is no task that has gone uncompleted.</p>
+              </div>
+              <div>
+                <img src={world} alt="world" />
+                <h5>Any Location</h5>
+                <p>We provide task around the world</p>
+              </div>
+            </section>
+    
+            <article className='middle'>
+              <h2>How is this possible?</h2>
+              <div>
+                <div>
+                  <img src={notebook} alt="notebook" />
+                </div>
+                <div>
+                  <h5>Title</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <img src={map} alt="map" />
+                </div>
+                <div>
+                  <h5>Title</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <img src={painting} alt="painting" />
+                </div>
+                <div>
+                  <h5>Title</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+            </article>
+    
+    
+            <article className='end'>
+              <h1>What are you waiting for?</h1>
+              <h4>Get started today.</h4>
+              <div>
+                <a className="waves-effect waves-light btn-large" href="/">Sign in</a>
+                <a className="waves-effect waves-light btn-large" href="/">Sign up</a>
+              </div>
+            </article>
+          </main>
+      );
+    
   }
 }
-*/
-
-const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit,
-    '&:focus':{
-      "background-color": "none !important",
-    }
-  },
-  green:{
-    "font-size": "large",
-    margin: theme.spacing.unit,
-    color: "#6cea71ab",
-    '&:hover':{
-      color: "white",
-      background: "rgba(255,255,255, 0.4)",
-      "transition-timing-function": "ease" 
-    },
-    "@media only screen and (max-width: 600px)": {
-      color: "white",
-      "&:active":{
-        background: "#6cea71ab"
-      },
-      "&:focus":{
-        background: "#6cea71ab"
-      }
-    }
-  },
-  blue:{
-    "font-size": "large",
-    margin: theme.spacing.unit,
-    color: "#90caf9",
-    '&:hover':{
-      color: "white",
-      background: "rgba(255,255,255, 0.4)",
-    },
-    "@media only screen and (max-width: 600px)": {
-      color: "white",
-      "&:active":{
-        background: "#90caf9"
-      },
-      "&:focus":{
-        background: "#90caf9"
-      }
-    }
-  },
-});
-
-function Navbar(props) {
-  const { classes } = props;
-  return (
-    <main className="About">
-        <div>
-          <div className="hero-image">
-            <div className='hero-text'>
-              <div className="center-align">We will let you free from your duties.</div>
-                <h4 className="center-align">Let someone else do your job.</h4>
-                <form className="col s12">
-                  <div className="input-field col s6">
-                    <input id="about-input" type="text" className="about-input"/>
-                    <label  className="center-align about-input-label" htmlFor="about-input">What do you need?</label>
-                  </div>
-                </form>
-                <div className="about-buttons-space-around">
-                    <Button size="large" className={classes.green}><Icon>create</Icon> Create</Button>
-                    <Button size="large" className={classes.blue} ><Icon>search</Icon> Search</Button>
-                    </div>
-                {/* <div className="about-buttons-space-around">
-                    <button className="waves-effect green lighten-1 btn-large" >Create</button>
-                    <button className="waves-effect blue lighten-3 btn-large" >Search</button>
-                </div> */}
-            </div>
-          </div>
-        </div>  
-         
-
-        <section>
-          <div>
-            <img src={check} alt="check" />
-            <h5>Reliable Taskers</h5>
-            <p>Task perfomers verified through us.</p>
-          </div>
-          <div>
-            <img src={clipboard} alt="clipboard" />
-            <h5>Amazing Reviews</h5>
-            <p>We ensure every customer receives satifactory work.</p>
-          </div>
-          <div>
-            <img src={target} alt="target" />
-            <h5>Perfect Score</h5>
-            <p>There is no task that has gone uncompleted.</p>
-          </div>
-          <div>
-            <img src={world} alt="world" />
-            <h5>Any Location</h5>
-            <p>We provide task around the world</p>
-          </div>
-        </section>
-
-        <article className='middle'>
-          <h2>How is this possible?</h2>
-          <div>
-            <div>
-              <img src={notebook} alt="notebook" />
-            </div>
-            <div>
-              <h5>Title</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <img src={map} alt="map" />
-            </div>
-            <div>
-              <h5>Title</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <img src={painting} alt="painting" />
-            </div>
-            <div>
-              <h5>Title</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-          </div>
-        </article>
 
 
-        <article className='end'>
-          <h1>What are you waiting for?</h1>
-          <h4>Get started today.</h4>
-          <div>
-            <a className="waves-effect waves-light btn-large" href="/">Sign in</a>
-            <a className="waves-effect waves-light btn-large" href="/">Sign up</a>
-          </div>
-        </article>
-      </main>
-  );
-}
-
-Navbar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Navbar);
