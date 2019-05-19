ad7cbdbbda7041dba4ae756afffcd7b061f610f0
in43sh <in43sh@gmail.com>
2019-05-18 20:26:21 -0700

    Change deprecated findById to findByPk in all controllers.

--- server/controllers/tasks_controller.js
+++ server/controllers/tasks_controller.js
@@ -2,81 +2,81 @@ const db = require('../models');
 const { Task } = db;
 
 module.exports = {
-  createTask: (req, res, next) => {
-    const { title, category, creator_id, contractor_id, price, description, zip, deadline, status, photos, requests } = req.body;
-    return Task
-    .create({ title, category, creator_id, contractor_id, price, description, zip, deadline, status, photos, requests })
-    .then(task => res.status(200).json({ status: 'Created one task', task }))
-    .catch(error => console.log(error));
-  },
-
-  getAllTasks: (req, res, next) => {
-    return Task
-    .findAll({paranoid: false})
-    .then(tasks => res.status(200).json({ status: 'Retrieved all tasks', tasks }))
-    .catch(error => console.log(error));
-  },
-
-  getSingleTask: (req, res, next) => {
-    const { id } = req.params;
-    return Task
-    .findById(id)
-    .then(task => res.status(200).json({ status: 'Retrieved one task', task }))
-    .catch(error => console.log(error));
-  },
-
-    getTasksByUserId: (req, res, next) => {
-      const { user_id } = req.params;
-      return Task
-      .findAll({
-        where: {creator_id: user_id}
-      })
-      .then(tasks => res.status(200).json({ status: 'Retrieved tasks for the user', tasks}))
-      .catch(error => console.log(error));
+    createTask: (req, res, next) => {
+        const { title, category, creator_id, contractor_id, price, description, zip, deadline, status, photos, requests } = req.body;
+        return Task
+            .create({ title, category, creator_id, contractor_id, price, description, zip, deadline, status, photos, requests })
+            .then(task => res.status(200).json({ status: 'Created one task', task }))
+            .catch(error => console.log(error));
     },
 
-  updateTask: (req, res, next) => {
-    const { id } = req.params;
-    const { title, category, creator_id, contractor_id, price, description, zip, deadline, status, photos, requests } = req.body;
-    return Task
-    .findById(id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send({ message: 'Task not found' })
-      }
-      return task
-      .update({
-        title: title,
-        category: category,
-        creator_id: creator_id,
-        contractor_id: contractor_id,
-        price: price,
-        description: description,
-        zip: zip,
-        deadline: deadline,
-        status: status,
-        photos: photos,
-        requests: requests
-      })
-      .then(task => res.status(200).json({ status: 'Updated one task', task }))
-      .catch(error => console.log(error));
-    })
-    .catch(error => console.log(error));
-  },
+    getAllTasks: (req, res, next) => {
+        return Task
+            .findAll({ paranoid: false })
+            .then(tasks => res.status(200).json({ status: 'Retrieved all tasks', tasks }))
+            .catch(error => console.log(error));
+    },
 
-  destroyTask: (req, res, next) => {
-    const { id } = req.params;
-    return Task
-    .findById(id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send({ message: 'Task not found' })
-      }
-      return task
-      .destroy()
-      .then(() => res.status(200).json({ status: 'Deleted one task', task }))
-      .catch(error => console.log(error));
-    })
-    .catch(error => console.log(error));
-  }
+    getSingleTask: (req, res, next) => {
+        const { id } = req.params;
+        return Task
+            .findByPk(id)
+            .then(task => res.status(200).json({ status: 'Retrieved one task', task }))
+            .catch(error => console.log(error));
+    },
+
+    getTasksByUserId: (req, res, next) => {
+        const { user_id } = req.params;
+        return Task
+            .findAll({
+                where: { creator_id: user_id }
+            })
+            .then(tasks => res.status(200).json({ status: 'Retrieved tasks for the user', tasks }))
+            .catch(error => console.log(error));
+    },
+
+    updateTask: (req, res, next) => {
+        const { id } = req.params;
+        const { title, category, creator_id, contractor_id, price, description, zip, deadline, status, photos, requests } = req.body;
+        return Task
+            .findById(id)
+            .then(task => {
+                if (!task) {
+                    return res.status(404).send({ message: 'Task not found' })
+                }
+                return task
+                    .update({
+                        title: title,
+                        category: category,
+                        creator_id: creator_id,
+                        contractor_id: contractor_id,
+                        price: price,
+                        description: description,
+                        zip: zip,
+                        deadline: deadline,
+                        status: status,
+                        photos: photos,
+                        requests: requests
+                    })
+                    .then(task => res.status(200).json({ status: 'Updated one task', task }))
+                    .catch(error => console.log(error));
+            })
+            .catch(error => console.log(error));
+    },
+
+    destroyTask: (req, res, next) => {
+        const { id } = req.params;
+        return Task
+            .findById(id)
+            .then(task => {
+                if (!task) {
+                    return res.status(404).send({ message: 'Task not found' })
+                }
+                return task
+                    .destroy()
+                    .then(() => res.status(200).json({ status: 'Deleted one task', task }))
+                    .catch(error => console.log(error));
+            })
+            .catch(error => console.log(error));
+    }
 };
--- server/controllers/users_controller.js
+++ server/controllers/users_controller.js
@@ -1,124 +1,123 @@
-
 const db = require('../models');
-const bcrypt = require ('bcrypt');
+const bcrypt = require('bcrypt');
 const saltRound = 12;
 const { User } = db;
 
 module.exports = {
-  register: (req, res, next) => {
-    const { nickname, email, ip, zip, personal, rating, avatar } = req.body;
-    let { password } = req.body;
-    // using bcrypt to encrypt the password
-    bcrypt.hash(password, saltRound)
-    .then(hashedPassword => {
-      password = hashedPassword;
-      return User
-      .create({ nickname, password, email, ip, zip, personal, rating, avatar })
-      .then(user => res.status(200).json({ status: 'Created one user', user }))
-      .catch(error => console.log(error));
-    })
-    .catch(error => console.log(error));
-  },
+    register: (req, res, next) => {
+        const { nickname, email, ip, zip, personal, rating, avatar } = req.body;
+        let { password } = req.body;
+        // using bcrypt to encrypt the password
+        bcrypt.hash(password, saltRound)
+            .then(hashedPassword => {
+                password = hashedPassword;
+                return User
+                    .create({ nickname, password, email, ip, zip, personal, rating, avatar })
+                    .then(user => res.status(200).json({ status: 'Created one user', user }))
+                    .catch(error => console.log(error));
+            })
+            .catch(error => console.log(error));
+    },
 
-  login: (req, res, next) => {
-    // getting user from the request
-    const { nickname, password } = req.body;
-    return User
-    // looking for a user with the nickname from the request
-    .findAll({
-      where: {
-        nickname: nickname
-      }
-    })
-    .then(users => {
-      // if there is no user with a requested nickname we return error 403
-      if (users.length) {
-        bcrypt.compare(password, users[0].dataValues.password).then(passwordMatch => {
-          // if the password does not correspond with one in database we return error 401
-          if (passwordMatch) {
-            req.session.user = {
-              nickname: users[0].dataValues.nickname
-            }
-            // if the user was not initialized we create it in checkForSession middleware
-            res.status(200).json( req.session.user );
-          } else {
-            res.status(401).json({ message: 'Wrong password' });
-          }
-        })
-        .catch((error) => res.status(500).send(error));
-      } else {
-        res.status(403).json({ message: "That user is not registered" });
-      }
-    })
-    .catch(error => res.status(500).send(error));
-  },
+    login: (req, res, next) => {
+        // getting user from the request
+        const { nickname, password } = req.body;
+        return User
+            // looking for a user with the nickname from the request
+            .findAll({
+                where: {
+                    nickname: nickname
+                }
+            })
+            .then(users => {
+                // if there is no user with a requested nickname we return error 403
+                if (users.length) {
+                    bcrypt.compare(password, users[0].dataValues.password).then(passwordMatch => {
+                            // if the password does not correspond with one in database we return error 401
+                            if (passwordMatch) {
+                                req.session.user = {
+                                        nickname: users[0].dataValues.nickname
+                                    }
+                                    // if the user was not initialized we create it in checkForSession middleware
+                                res.status(200).json(req.session.user);
+                            } else {
+                                res.status(401).json({ message: 'Wrong password' });
+                            }
+                        })
+                        .catch((error) => res.status(500).send(error));
+                } else {
+                    res.status(403).json({ message: "That user is not registered" });
+                }
+            })
+            .catch(error => res.status(500).send(error));
+    },
 
-  logout: (req, res, next) => {
-    req.session.destroy();
-    res.status(200).json( { message: 'User is logged out'} );
-  },
+    logout: (req, res, next) => {
+        req.session.destroy();
+        res.status(200).json({ message: 'User is logged out' });
+    },
 
-  createUser: (req, res, next) => {
-    const { nickname, password, email, ip, zip, personal, rating, avatar } = req.body;
-    return User
-    .create({ nickname, password, email, ip, zip, personal, rating, avatar })
-    .then(user => res.status(200).json({ status: 'Created one user', user }))
-    .catch(error => console.log(error));
-  },
+    createUser: (req, res, next) => {
+        const { nickname, password, email, ip, zip, personal, rating, avatar } = req.body;
+        return User
+            .create({ nickname, password, email, ip, zip, personal, rating, avatar })
+            .then(user => res.status(200).json({ status: 'Created one user', user }))
+            .catch(error => console.log(error));
+    },
 
-  getAllUsers: (req, res, next) => {
-    return User
-    .findAll({paranoid: false})
-    .then(users => res.status(200).json({ status: 'Retrieved all users', users }))
-    .catch(error => console.log(error));
-  },
+    getAllUsers: (req, res, next) => {
+        return User
+            .findAll({ paranoid: false })
+            .then(users => res.status(200).json({ status: 'Retrieved all users', users }))
+            .catch(error => console.log(error));
+    },
 
-  getSingleUser: (req, res, next) => {
-    const { id } = req.params;
-    return User
-    .findById(id)
-    .then(user => res.status(200).json({ status: 'Retrieved one user', user }))
-    .catch(error => console.log(error));
-  },
+    getSingleUser: (req, res, next) => {
+        const { id } = req.params;
+        return User
+            .findByPk(id)
+            .then(user => res.status(200).json({ status: 'Retrieved one user', user }))
+            .catch(error => console.log(error));
+    },
 
-  updateUser: (req, res, next) => {
-    const { id } = req.params;
-    const { nickname, password, email, ip, zip, personal, rating, avatar } = req.body;
-    return User
-    .findById(id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send({ message: 'User not found' })
-      }
-      return user
-      .update({
-        nickname: nickname,
-        email: email,
-        ip: ip,
-        zip: zip,
-        personal: personal,
-        rating: rating,
-        avatar: avatar
-      })
-      .then(user => res.status(200).json({ status: 'Updated one user', user }))
-      .catch(error => console.log(error));
-    })
-    .catch(error => console.log(error));
-  },
+    updateUser: (req, res, next) => {
+        const { id } = req.params;
+        const { nickname, password, email, ip, zip, personal, rating, avatar } = req.body;
+        return User
+            .findByPk(id)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send({ message: 'User not found' })
+                }
+                return user
+                    .update({
+                        nickname: nickname,
+                        email: email,
+                        ip: ip,
+                        zip: zip,
+                        personal: personal,
+                        rating: rating,
+                        avatar: avatar
+                    })
+                    .then(user => res.status(200).json({ status: 'Updated one user', user }))
+                    .catch(error => console.log(error));
+            })
+            .catch(error => console.log(error));
+    },
 
-  destroyUser: (req, res, next) => {
-    const { id } = req.params;
-    return User
-    .findById(id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send({ message: 'User not found' })
-      }
-      return user
-      .destroy()
-      .then(() => res.status(200).json({ status: 'Deleted one user', user }))
-      .catch(error => console.log(error));
-    })
-    .catch(error => console.log(error));
-  }
+    destroyUser: (req, res, next) => {
+        const { id } = req.params;
+        return User
+            .findByPk(id)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send({ message: 'User not found' })
+                }
+                return user
+                    .destroy()
+                    .then(() => res.status(200).json({ status: 'Deleted one user', user }))
+                    .catch(error => console.log(error));
+            })
+            .catch(error => console.log(error));
+    }
 };
